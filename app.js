fa2951a946e8a4304b430999c7984b90bece6c29
Pooria Rezaee <pooriarezaee.dev@gmail.com>
2026-07-24 16:47:21 +0330

    fix :

---
+++ app.js
@@ -0,0 +1,241 @@
+const sec = document.querySelector(".sec")
+const btn = document.getElementById("btn")
+const btn2 = document.getElementById("btn2")
+const div = document.querySelectorAll(".sec>button")
+const h2 = document.getElementById("h2")
+const h3 = document.getElementById("h3")
+const heart = document.querySelector(".heart")
+const sp = document.querySelector(".sp")
+const tr = new Audio("audio/Hammer_Nail_Hit_4.mp3")
+const missed = new Audio("audio/Wrong Answer Sound Effect.mp3")
+const win = new Audio("audioVictory Sound Effects 4.mp3")
+const lose = new Audio("audio/Sad Trombone Wah Wah Wah Fail Sound Effect.mp3")
+const record = document.querySelector("#record")
+
+
+
+
+let timer = 30
+let x
+let timer_random
+let ran
+let clicky = 0
+let joon = 5
+
+
+
+let save = localStorage.getItem("unique2")
+
+record.innerHTML = ` ${save || 0}`
+
+
+
+div.forEach((val) => {
+    val.setAttribute("disabled", "disabled")
+})
+
+function random() {
+
+    timer_random = setInterval(() => {
+
+        setTimeout(() => {
+
+            ran = parseInt(Math.random() * 9) + 1
+
+            div.forEach((val, index) => {
+
+                if (index == ran) {
+
+                    val.innerHTML = "🐭"
+
+                } else {
+
+                    val.innerHTML = "🕳️"
+
+                }
+
+            })
+
+        }, 550)
+
+    }, 750)
+
+    f()
+
+}
+
+btn.addEventListener("click", () => {
+
+    div.forEach((val) => {
+        val.removeAttribute("disabled")
+    })
+
+    if (timer > 0) {
+
+        x = setInterval(() => {
+
+            timer--
+
+            h2.innerHTML = `⏱ ${timer}s`
+
+            if (timer == -1) {
+
+                alert("⏰ Time's up! Game Over!")
+
+                clearInterval(x)
+                clearInterval(timer_random)
+
+                timer = 30
+                clicky = 0
+
+                h2.innerHTML = "⏱ 30s"
+                h3.innerHTML = clicky
+
+                btn.removeAttribute("disabled")
+                clone()
+                location.reload()
+
+            }
+
+        }, 1000)
+
+        btn.setAttribute("disabled", "disabled")
+
+        random()
+
+    }
+
+})
+
+btn2.addEventListener("click", () => {
+
+    alert("🔄 The game has been restarted.")
+
+    clearInterval(x)
+    clearInterval(timer_random)
+    clone()
+    timer = 30
+    clicky = 0
+
+    h2.innerHTML = "⏱ 30s"
+    h3.innerHTML = clicky
+
+    timer_random = null
+
+    btn.removeAttribute("disabled")
+
+    div.forEach((val) => {
+        val.setAttribute("disabled", "disabled")
+    })
+    heart.innerHTML = "❤️ ❤️ ❤️ ❤️ ❤️"
+
+    location.reload()
+
+})
+
+function f() {
+
+    div.forEach((val, index) => {
+
+        val.addEventListener("dblclick", () => {
+
+            if (clicky == 5) {
+
+                alert("🎉 Congratulations! You caught all 7 mice!")
+                win.play()
+                win.currentTime = 0
+                setTimeout(() => {
+                    location.reload()
+                }, 1000);
+
+            }
+
+            if (index == ran) {
+
+                clicky++
+
+                h3.innerHTML = clicky
+
+                tr.play()
+                tr.currentTime = 0
+
+
+
+            } else {
+
+
+                joon--
+
+                if (joon == 4) {
+
+                    heart.innerHTML = "❤️ ❤️ ❤️ ❤️ 🖤"
+                    missed.play()
+                    missed.currentTime = 0
+
+                } else if (joon == 3) {
+
+                    heart.innerHTML = "❤️ ❤️ ❤️ 🖤 🖤"
+                    missed.play()
+                    missed.currentTime = 0
+
+                } else if (joon == 2) {
+
+                    heart.innerHTML = "❤️ ❤️ 🖤 🖤 🖤"
+                    missed.play()
+                    missed.currentTime = 0
+
+                } else if (joon == 1) {
+
+                    heart.innerHTML = "❤️ 🖤 🖤 🖤 🖤"
+                    missed.play()
+                    missed.currentTime = 0
+
+                } else if (joon == 0) {
+
+                    heart.innerHTML = "🖤 🖤 🖤 🖤 🖤"
+                    lose.play()
+                    lose.currentTime = 0
+                    alert("💔 Game Over! You lost all your hearts.")
+                    clone()
+                    setTimeout(() => {
+
+                        location.reload()
+
+                    }, 1000)
+
+                }
+
+            }
+
+        })
+
+    })
+
+}
+
+
+
+document.addEventListener("mousemove", (e) => {
+
+    let x = e.clientX
+    let y = e.clientY
+    sp.style.left = `${x}px`
+    sp.style.top = `${y}px`
+
+})
+
+function clone() {
+
+    let save = localStorage.getItem("unique2")
+
+    if (save == null || clicky > Number(save)) {
+
+        localStorage.setItem("unique2", clicky)
+
+    }
+
+}
+
+
+
+
